d5b010b082720a2fa797e9ac5af3a21e611fd3db
Vipin <vipinkr3000@gmail.com>
2022-07-01 12:02:32 +0530

    Adding indentation to the code

--- pc_health_check.py
+++ pc_health_check.py
@@ -3,18 +3,19 @@
 import shutil
 import psutil
 
+
 def disk_usage(disk):
     du = shutil.disk_usage(disk)
     free = (du.free / du.total) * 100
     return free > 20  # Disk space should be more than 20%
 
+
 def cpu_usage():
     usage = psutil.cpu_percent(1)
     return usage < 75  # CPU usage should be less than 75%
+
 
 if not disk_usage("/") or not cpu_usage():
     print("ERROR!")
 else:
     print("Everything is OK!")
-
-
